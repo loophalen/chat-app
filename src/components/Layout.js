70db931487d4e501eec6047e27b0f05cea9f908c
blandhalen <blandhalen@blands-imac.home>
2018-08-17 18:54:30 -0400

    connected

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
+import { isNull } from 'util';
+
+
+const socketUrl = "http://192.168.1.7:3231"
 
 export default class Layout extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            socket:isNull
+        }
+    }
+
+    componentWillMount(){
+        this.initSocket()
+    }
+
+    initSocket = () => {
+        const socket = io(socketUrl)
+        socket.on('connect', ()=>{
+            console.log("connected")
+        })
+        this.setState({socket})
+    }
+
     render(){
         const { title } = this.props 
         return (
